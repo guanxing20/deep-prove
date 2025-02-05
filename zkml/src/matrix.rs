c498fe5677b091213caf6de03d0c8c90b8a944ad
nicolas gailly <nikkolasg@gmail.com>
2025-02-05 16:19:12 +0100

    mle evaluation

---
+++ zkml/src/matrix.rs
@@ -0,0 +1,197 @@
+use std::fmt::{Debug, Display};
+
+use ark_std::rand::{self, RngCore};
+use ff_ext::{ExtensionField, ff::Field};
+use itertools::Itertools;
+use multilinear_extensions::mle::{DenseMultilinearExtension, MultilinearExtension};
+use rayon::iter::{IndexedParallelIterator, IntoParallelIterator, ParallelIterator};
+
+#[derive(Clone)]
+pub struct Matrix<E> {
+    dim: (usize, usize),
+    // dimension is [n_rows,n_cols]
+    coeffs: Vec<Vec<E>>,
+}
+
+impl<E> Matrix<E>
+where
+    E: ExtensionField,
+{
+    /// From a given row and a given column, return the vector of field elements in the right
+    /// format to evaluate the MLE.
+    /// little endian so we need to read cols before rows
+    pub fn xy_to_boolean(&self, row: usize, col: usize) -> Vec<E> {
+        to_bit_sequence_le(col, self.ncols().ilog2() as usize)
+            .chain(to_bit_sequence_le(row, self.nrows().ilog2() as usize))
+            .map(|bit| E::from(bit as u64))
+            .collect_vec()
+    }
+
+    pub fn to_mle(self) -> impl MultilinearExtension<E> {
+        assert!(
+            self.nrows().is_power_of_two(),
+            "number of rows {} is not a power of two",
+            self.nrows()
+        );
+        assert!(
+            self.ncols().is_power_of_two(),
+            "number of columns {} is not a power of two",
+            self.ncols()
+        );
+        // N variable to address 2^N rows and M variables to address 2^M columns
+        let num_vars = self.nrows().ilog2() + self.ncols().ilog2();
+        DenseMultilinearExtension::from_evaluation_vec_smart(
+            num_vars as usize,
+            self.coeffs.into_par_iter().flatten().collect(),
+        )
+    }
+    pub fn pad_next_power_of_two(mut self) -> Self {
+        // assume the matrix is already well formed and there is always n_rows and n_cols
+        // this is because we control the creation of the matrix in the first place
+        let new_rows = if self.nrows().is_power_of_two() {
+            self.nrows()
+        } else {
+            self.nrows().next_power_of_two()
+        };
+        let new_cols = if self.ncols().is_power_of_two() {
+            self.ncols()
+        } else {
+            self.ncols().next_power_of_two()
+        };
+        self.dim = (new_rows, new_cols);
+        // resize each row
+        for row in self.coeffs.iter_mut() {
+            if row.len() != new_cols {
+                row.resize(new_cols, E::ZERO);
+            }
+        }
+        // resize the number of rows
+        if self.coeffs.len() != new_rows {
+            self.coeffs.resize(new_rows, vec![E::ZERO; new_cols]);
+        }
+        self
+    }
+    /// Creates a random matrix with a given number of rows and cols.
+    /// NOTE: doesn't take a rng as argument because to generate it in parallel it needs be sync +
+    /// sync which is not true for basic rng core.
+    pub fn random((rows, cols): (usize, usize)) -> Self {
+        let coeffs = (0..rows * cols)
+            .into_par_iter()
+            .map(|i| {
+                // let mut rng = rand::thread_rng();
+                // E::random(&mut rng)
+                E::from(i as u64)
+            })
+            .chunks(cols)
+            .collect();
+        Self {
+            dim: (rows, cols),
+            coeffs,
+        }
+    }
+    pub fn nrows(&self) -> usize {
+        self.dim.0
+    }
+    pub fn ncols(&self) -> usize {
+        self.dim.1
+    }
+}
+
+impl<E: ExtensionField> Debug for Matrix<E> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Matrix({},{})\n", self.nrows(), self.ncols())?;
+        for (i, row) in self.coeffs.iter().enumerate() {
+            let row_int = row.iter().map(|c| c.to_canonical_u64_vec()).collect_vec();
+            write!(f, "{}: {:?}\n", i, row_int)?;
+        }
+        Ok(())
+    }
+}
+
+fn to_bit_sequence_le(num: usize, bit_length: usize) -> impl Iterator<Item = usize> {
+    assert!(
+        bit_length as u32 <= usize::BITS,
+        "bit_length cannot exceed usize::BITS"
+    );
+    (0..bit_length).map(move |i| ((num >> i) & 1) as usize)
+}
+
+#[cfg(test)]
+mod test {
+    use ark_std::rand::{Rng, thread_rng};
+    use ff::Field;
+    use ff_ext::ExtensionField;
+    use goldilocks::{Goldilocks, GoldilocksExt2};
+    use itertools::Itertools;
+    use multilinear_extensions::mle::MultilinearExtension;
+
+    use crate::matrix::to_bit_sequence_le;
+
+    use super::Matrix;
+
+    impl<E> Matrix<E>
+    where
+        E: ExtensionField,
+    {
+        pub fn assert_structure(&self, (n_rows, n_cols): (usize, usize)) {
+            assert_eq!(self.dim.0, n_rows);
+            assert_eq!(self.dim.1, n_cols);
+            assert_eq!(n_rows, self.coeffs.len());
+            for row in &self.coeffs {
+                assert_eq!(n_cols, row.len());
+            }
+        }
+        pub fn get(&self, i: usize, j: usize) -> E {
+            self.coeffs[i][j].clone()
+        }
+    }
+
+    pub fn to_bit_sequence_be(num: usize, bit_length: usize) -> impl Iterator<Item = usize> {
+        assert!(
+            bit_length as u32 <= usize::BITS,
+            "bit_length cannot exceed usize::BITS"
+        );
+        ((usize::BITS - bit_length as u32)..usize::BITS)
+            .rev()
+            .map(move |i| ((num >> i) & 1) as usize)
+    }
+
+    #[test]
+    fn test_matrix_mle() {
+        type E = GoldilocksExt2;
+        let mat = Matrix::<E>::random((4, 4)).pad_next_power_of_two();
+        println!("matrix: {:?}", mat);
+        let mle = mat.clone().to_mle();
+        let (elem_x, elem_y) = (
+            thread_rng().gen_range(0..mat.dim.0),
+            thread_rng().gen_range(0..mat.dim.1),
+        );
+        let elem = mat.get(elem_x, elem_y);
+        println!(
+            "(x,y) = ({},{}) ==> {:?} ({:?})",
+            elem_x,
+            elem_y,
+            elem.to_canonical_u64_vec(),
+            elem
+        );
+        let inputs = mat.xy_to_boolean(elem_x, elem_y);
+        let output = mle.evaluate(&inputs);
+        assert_eq!(elem, output);
+    }
+
+    #[test]
+    fn test_matrix_random() {
+        let (n_rows, n_cols) = (10, 10);
+        let mat = Matrix::<GoldilocksExt2>::random((n_rows, n_cols));
+        mat.assert_structure((n_rows, n_cols));
+    }
+
+    #[test]
+    fn test_matrix_next_power_of_two() {
+        let (n_rows, n_cols) = (10, 10);
+        let mat = Matrix::<GoldilocksExt2>::random((n_rows, n_cols));
+        let (new_rows, new_cols) = (n_rows.next_power_of_two(), n_cols.next_power_of_two());
+        let new_mat = mat.pad_next_power_of_two();
+        new_mat.assert_structure((new_rows, new_cols));
+    }
+}
