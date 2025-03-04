3592e17c2d507dc859eacab103c2bbf103230aae
nicolas gailly <nikkolasg@gmail.com>
2025-03-04 23:50:11 +0100

    proper fix

--- zkml/src/quantization.rs
+++ zkml/src/quantization.rs
@@ -134,20 +134,21 @@ impl<const BIT_LEN: usize> QuantRange<BIT_LEN> {
             .chunks(ncols)
             .into_iter()
             .map(|row| {
-                let row_range = row.map(|weight| {
-                    let min = if weight.is_negative() {
-                        weight * MAX as Element
-                    } else {
-                        weight * MIN as Element
-                    };
-                    let max = if weight.is_negative() {
-                        weight * MIN as Element
-                    } else {
-                        weight * MAX as Element
-                    };
-                    (min, max)
-                })
-                .fold((0, 0), |(min, max), (wmin, wmax)| (min + wmin, max + wmax));
+                let row_range = row
+                    .map(|weight| {
+                        let min = if weight.is_negative() {
+                            weight * MAX as Element
+                        } else {
+                            weight * MIN as Element
+                        };
+                        let max = if weight.is_negative() {
+                            weight * MIN as Element
+                        } else {
+                            weight * MAX as Element
+                        };
+                        (min, max)
+                    })
+                    .fold((0, 0), |(min, max), (wmin, wmax)| (min + wmin, max + wmax));
                 // weight * MIN can be positive and higher then MAX*weight if weight's negative
                 // so we take the absolute value of the difference
                 (row_range.1 - row_range.0).unsigned_abs() as usize
